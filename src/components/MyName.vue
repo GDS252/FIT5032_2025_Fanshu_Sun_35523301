c5e516afe571cb10734e20ed476e0fbcf5025914
SEBEDKLK <1085244308@qq.com>
2025-06-24 19:11:33 +0800

    Initial commit - Vue.js project

---
+++ src/components/MyName.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Hello from Fanshu Sun!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+h2 {
+  color: red !important; 
+  background-color: yellow !important; 
+  font-size: 3em !important; 
+  border: 5px solid blue !important; 
+  padding: 20px !important; 
+  margin-top: 50px !important; 
+}
+</style>
