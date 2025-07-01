c5db1f1b184d31552b1af35221334396bfa3b350
Fanshu Sun <fsun0025@student.monash.edu>
2025-07-01 16:57:35 +0800

    fit5032-week2

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import JSONLab from './components/JSON.vue'
+</script>
+
+<template>
+  <div id="app">
+    <JSONLab />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  background-color: #ffffff;
+  color: #333333;
+  box-sizing: border-box;
+}
+</style>
