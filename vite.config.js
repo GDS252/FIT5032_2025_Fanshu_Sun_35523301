99acd49d55b4c4f60c95d2b8298b4bbcec281680
GDS252 <fsun0025@student.monash.edu>
2025-08-12 16:12:58 +0800

    Fix vite.config.js syntax error

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
+  },
+  base: process.env.NODE_ENV === 'production'
     ? '/FIT5032_2025_Fanshu_Sun_35523301/'
     : '/'
-}
+})
